838a11f5dec70a9d905e78f8704236d94a553886
stefangh <stefangh@gmail.com>
2011-09-06 16:01:01 +0000

    solved (repaired) 1435.cpp

--- done/1435.cpp
+++ done/1435.cpp
@@ -3,68 +3,71 @@
  *  Financial Error - Problem 1435
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
-#define input	"file.in"
-#define MAXN	1500100L
+#define MAXN	200000L
 
-long N;
-long A[MAXN];
-__int64 Sum, Total, Delta;
+int N;
+long long A[MAXN];
+long long Sum, Total, Delta;
+long long pow10[64];
 
 
-int SwitchDigits(__int64 &num, int p1, int p2)
+int SwitchDigits(long long &num, int p1, int p2)
 {
-	char n[32], ch;
+  int n = 0;
+  long long on = num;
 
-	_ltoa(num, n, 10);
+  // count digits
+  while (on > 0)
+  {
+    on /= 10;
+    n++;
+  }
 
-	if (p1 >= strlen(n) || p2 >= strlen(n)) 
-		return 1;
+  if (p1 >= n || p2 >= n || p1 < 0 || p2 < 0)
+    return 1;
 
-	_strrev(n);
+  // digit p1 and p2
+  int c1 = (num / pow10[p1]) % 10;
+  int c2 = (num / pow10[p2]) % 10;
 
-	ch = n[p1];
-	n[p1] = n[p2];
-	n[p2] = ch;
-	
-	if (n[p2] == '0' && p2 == strlen(n) - 1)
-		return 1;
+  if (c1 == 0 && p2 == n - 1)
+    return 1;
 
-	_strrev(n);
+  num += pow10[p1] * (c2 - c1);
+  num += pow10[p2] * (c1 - c2);
 
-	num = _atoi64(n);
-
-	return 0;
+  return 0;
 }
 
 int main()
 {
-	long i;
+	int i, pos;
+
+  pow10[0] = 1;
+  for (i = 1; i < 64; i++)
+    pow10[i] = pow10[i - 1] * 10;
 
 #ifndef ONLINE_JUDGE
-	freopen(input, "rt", stdin);
+	freopen("input.txt", "rt", stdin);
 #endif
 	scanf("%d\n", &N);
 
 	Total = 0;
 	for (i = 0; i < N; i++)
 	{
-		char num[16];
-		gets(num);
-
-		A[i] = atol(num);
-
-		//scanf("%ld", A + i);
-		Total += (__int64)A[i];
+		scanf("%lld", &A[i]);
+		Total += A[i];
 	}
-	scanf("%I64d", &Sum);
+	scanf("%lld", &Sum);
 
 	Delta = Sum - Total;
 
-	if (Delta < 0) Delta *= -1;
+	if (Delta < 0) 
+    Delta *= -1;
 
 	if (Delta == 0)
 	{
@@ -72,40 +75,48 @@ int main()
 		return 0;
 	}
 
-	int pos;
-	__int64 t1 = 9, t2 = 81;
+  long long t1 = 9, t2 = 81, j;
 
+  // check at what digit the mistake could have occured
 	for (pos = 0; pos < 1000; pos++)
 	{
-		if (t1 <= Delta && Delta <= t2) break;
+		if (t1 <= Delta && Delta <= t2) 
+      break;
 		t1 *= 10;
 		t2 *= 10;
 	}
 
 	for (i = 0; i < N; i++)
 	{
-		__int64 elem = A[i];
+		long long elem = A[i];
 
 		Total -= elem;
 
-		if (!SwitchDigits(elem, pos, pos + 1))
-		{
-			Total += elem;
-			if (Total == Sum)
-			{
-				printf("Error in record #%ld. Correct number is: %I64d.\n", i + 1, elem);
-				return 0;
-			}
-			Total -= elem;
+    // test also pos nearby digits (-1, +1)
+    for (j = pos - 1; j <= pos + 1; j++)
+    {
+      if (j < 0)
+        continue;
+  		if (!SwitchDigits(elem, j, j + 1))
+	  	{
+		  	Total += elem;
+			  if (Total == Sum)
+  			{
+	  			printf("Error in record #%d. Correct number is: %lld.\n", i + 1, elem);
+		  		return 0;
+			  }
+  			Total -= elem;
 
-			SwitchDigits(elem, pos, pos + 1);
-		}
+	  		SwitchDigits(elem, j, j + 1);
+		  }
+      else 
+        break;
+    }
 
-		Total += elem;
+  		Total += elem;
 	}
 
 	printf("Unrecoverable error.\n");
-
 
 	return 0;
 }
